68b7553960cba70aa9433a658b9564777031896f
SonSuss <8489son2311@gmail.com>
2025-06-29 08:36:10 +0700

    change downloader script

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -6,7 +6,17 @@ from datasets import load_dataset
 from tqdm import tqdm
 from huggingface_hub import hf_hub_download
 
-json_path = hf_hub_download(repo_id="mPLUG/TinyChartData",cache_dir='./data')
+repo_id = "mPLUG/TinyChartData"
+filenames = [
+    "tinychart_images.tar",
+    "train.json",
+    "test.json"
+]
 
+for filename in filenames:
+    if not os.path.exists(filename):
+        print(f"Downloading {filename}...")
+        hf_hub_download(repo_id=repo_id, filename=filename, cache_dir='./data')
+        
 if __name__ == "__main__":
     pass
