27a534b57617eed2a4878f3e3b831fcb531034f9
SonSuss <8489son2311@gmail.com>
2025-07-14 00:01:38 +0700

    aaaa

--- models/chart_qa_model/model/phi_4_llava.py
+++ models/chart_qa_model/model/phi_4_llava.py
@@ -85,40 +85,43 @@ class PhiLlavaForCausalLM(Phi3ForCausalLM, LlavaMetaForCausalLM):
     @torch.no_grad()
     def generate(
         self,
-        inputs: Optional[torch.Tensor] = None,
+        input_ids: Optional[torch.Tensor] = None,
         images: Optional[torch.Tensor] = None,
         **kwargs,
     ) -> torch.LongTensor:
-        position_ids = kwargs.pop("position_ids", None)
-        attention_mask = kwargs.pop("attention_mask", None)
-        if "inputs_embeds" in kwargs:
-            raise NotImplementedError("`inputs_embeds` is not supported")
-
         if images is not None:
+            # Only use custom logic when we have images
+            position_ids = kwargs.pop("position_ids", None)
+            attention_mask = kwargs.pop("attention_mask", None)
+            
             (
-                inputs,
+                input_ids,
                 position_ids,
                 attention_mask,
                 _,
                 inputs_embeds,
                 _
             ) = self.prepare_inputs_labels_for_multimodal(
-                inputs,
+                input_ids,
                 position_ids,
                 attention_mask,
                 None,
                 None,
                 images,
             )
+            
+            return super().generate(
+                position_ids=position_ids,
+                attention_mask=attention_mask,
+                inputs_embeds=inputs_embeds,
+                **kwargs
+            )
         else:
-            inputs_embeds = self.get_model().embed_tokens(inputs)
-
-        return super().generate(
-            position_ids=position_ids,
-            attention_mask=attention_mask,
-            inputs_embeds=inputs_embeds,
-            **kwargs
-        )
+            # For text-only generation, just use the parent class
+            return super().generate(
+                input_ids=input_ids,
+                **kwargs
+            )
 
     def prepare_inputs_for_generation(self, input_ids, past_key_values=None,
                                       inputs_embeds=None, **kwargs):
