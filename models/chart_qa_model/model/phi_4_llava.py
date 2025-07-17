ef44182870cd74729de085a87f6d2912d1d91c2c
SonSuss <8489son2311@gmail.com>
2025-07-17 20:08:19 +0700

    debug

--- models/chart_qa_model/model/phi_4_llava.py
+++ models/chart_qa_model/model/phi_4_llava.py
@@ -76,7 +76,7 @@ class PhiLlavaForCausalLM(Phi3ForCausalLM, LlavaMetaForCausalLM):
             attention_mask=attention_mask,
             position_ids=position_ids,
             past_key_values=past_key_values,
-            inputs_embeds=inputs_embeds,
+            # inputs_embeds=inputs_embeds,
             labels=labels,
             use_cache=use_cache,
             output_attentions=output_attentions,
