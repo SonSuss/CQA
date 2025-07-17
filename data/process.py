956af1cfcc0edb78a94e3d20c956fd9efce2383d
SonSuss <8489son2311@gmail.com>
2025-07-17 21:05:47 +0700

    debug

--- data/process.py
+++ data/process.py
@@ -75,9 +75,9 @@ def preprocess_multimodal(
                 sentence['value'] = sentence['value'].replace(DEFAULT_IMAGE_TOKEN, '').strip()
                 sentence['value'] = DEFAULT_IMAGE_TOKEN + '\n' + sentence['value']
                 sentence['value'] = sentence['value'].strip()
-                if "mmtag" in conversation_lib.default_conversation.version:
-                    sentence['value'] = sentence['value'].replace(DEFAULT_IMAGE_TOKEN,
-                                                                  '<Image>' + DEFAULT_IMAGE_TOKEN + '</Image>')
+                # if "mmtag" in conversation_lib.default_conversation.version:
+                #     sentence['value'] = sentence['value'].replace(DEFAULT_IMAGE_TOKEN,
+                #                                                   '<Image>' + DEFAULT_IMAGE_TOKEN + '</Image>')
             replace_token = DEFAULT_IMAGE_TOKEN
             if data_args.mm_use_im_start_end:
                 replace_token = DEFAULT_IM_START_TOKEN + replace_token + DEFAULT_IM_END_TOKEN
