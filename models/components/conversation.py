a6029bb9acad3993d66568e671680067475a3a29
SonSuss <8489son2311@gmail.com>
2025-07-18 18:28:15 +0700

    fix converstation

--- models/components/conversation.py
+++ models/components/conversation.py
@@ -415,7 +415,7 @@ Answer the questions.""",
 )
 
 conv_phi4_instruct = Conversation(
-    system="""Answer the question about the chart. 
+    system="""<|system|>\nAnswer the question about the chart. 
     First, think step by step and reason carefully to reach the solution. 
     Then, only return the brief and direct answer, without explanation.""",
     roles=("<|user|>", "<|assistant|>"),
