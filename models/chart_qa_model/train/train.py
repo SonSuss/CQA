b649b85f2392216e4035e8228ebc4aa67499bb50
SonSuss <8489son2311@gmail.com>
2025-07-18 23:55:05 +0700

    fix converstation

--- models/chart_qa_model/train/train.py
+++ models/chart_qa_model/train/train.py
@@ -113,11 +113,8 @@ def train(model_args:ModelArguments, data_args: DataArguments, training_args: Tr
         vision_tower.to(dtype=torch.bfloat16 if training_args.bf16 else torch.float16, device=training_args.device)
         
         if model_args.tune_vision_tower:
-            pass
-            # for p in model.get_vision_tower().parameters():
-                # p.requires_grad = True
-            # unlock_vit(training_args, model_args, vision_tower)
-            # logger.info(f"Vision tower frozen - trainable params: {sum(p.numel() for p in model.get_vision_tower().parameters() if p.requires_grad)}")
+            unlock_vit(training_args, model_args, vision_tower)
+            logger.info(f"Vision tower frozen - trainable params: {sum(p.numel() for p in model.get_vision_tower().parameters() if p.requires_grad)}")
 
         if training_args.gradient_checkpointing:
             vision_tower.vision_tower.gradient_checkpointing_enable(
