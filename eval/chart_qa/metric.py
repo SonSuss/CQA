9e6e71c62756877b51c636e9a608ff9354d6c013
SonSuss <8489son2311@gmail.com>
2025-07-31 14:46:38 +0700

    new eval

--- eval/chart_qa/metric.py
+++ eval/chart_qa/metric.py
@@ -18,13 +18,18 @@ def RelaxedAccuracy(pred, gt, threshold=0.05):
     except:
         ratio = difflib.SequenceMatcher(None, str(pred), str(gt)).ratio()
         return True if ratio >= 1-threshold else False
-        
+
+def clean_answer(ans):
+    # Remove trailing special tokens and whitespace
+    return ans.replace(" <|end|>", "").replace("<|end|>", "").strip()
 def chartqa_evaluator(data, key='final_model_answer'):
     relaxed_acc = 0
     standard_acc = 0
     for item in data:
-        item_relaxed_acc = RelaxedAccuracy(item[key], item['gt_answer'])
-        item_standard_acc = True if str(item[key]) == str(item['gt_answer']) else False
+        gt = clean_answer(str(item['gt_answer']))
+        pred = clean_answer(str(item[key]))
+        item_relaxed_acc = RelaxedAccuracy(pred, gt)
+        item_standard_acc = True if pred == gt else False
         if item_relaxed_acc:
             relaxed_acc += 1
         if item_standard_acc:
