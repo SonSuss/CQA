dd0640bf3199ea66fdde338bc0546842a0747f34
SonSuss <8489son2311@gmail.com>
2025-08-11 12:38:01 +0700

    init finetune

--- models/chart_qa_model/train/finetune.py
+++ models/chart_qa_model/train/finetune.py
@@ -43,5 +43,5 @@ def finetune(model_path: str, model_args: ModelArguments, data_args: DataArgumen
             trust_remote_code=True
         )
 
-    for name, module in model.named_children():
-        print(f"{name}: {module}")
+    for name, module in model.named_modules():
+        print(name, ":", type(module))
